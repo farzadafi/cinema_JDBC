7296381c8c622a893d66d4263cf348ada7d35187
farzad <farzadafi40@gmail.com>
2022-01-24 14:14:32 +0330

    add importAdmin method in AdminService

--- src/main/java/Service/AdminService.java
+++ src/main/java/Service/AdminService.java
@@ -1,4 +1,17 @@
 package Service;
 
+import Entity.Admin;
+import Repository.AdminRepository;
+
+import java.sql.SQLException;
+
 public class AdminService {
+    private AdminRepository adminRepository = new AdminRepository();
+
+    public AdminService() throws SQLException, ClassNotFoundException {
+    }
+
+    public int importAdmin(Admin admin) throws SQLException {
+        return adminRepository.importAdmin(admin);
+    }
 }

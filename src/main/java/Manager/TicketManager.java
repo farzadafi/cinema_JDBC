7c7103cd62899cf1cd8df58bd4842be3605b4acc
farzad <farzadafi40@gmail.com>
2022-01-24 20:25:53 +0330

    add reserveTicket to TicketManager

--- src/main/java/Manager/TicketManager.java
+++ src/main/java/Manager/TicketManager.java
@@ -1,5 +1,6 @@
 package Manager;
 
+import Entity.Basket;
 import Entity.Ticket;
 import Service.BasketService;
 import Service.CinemaService;
@@ -11,11 +12,12 @@ import java.sql.Time;
 import java.util.Scanner;
 
 public class TicketManager {
+    Manager manager = new Manager();
     private Scanner input = new Scanner(System.in);
     private TicketService ticketService = new TicketService();
     private CinemaService cinemaService = new CinemaService();
     private String cinemaName,filmName,timeDate,clock;
-    private int numberTickets,price;
+    private int numberTickets,price,priceAll;
     private BasketService basketService = new BasketService();
 
 
@@ -83,6 +85,61 @@ public class TicketManager {
 
     }
 
+    public void showAllTicket() throws SQLException {
+        ticketService.showAllTicket();
+    }
+
+    public void reserveTicket(String username) throws SQLException {
+        showAllTicket();
+        System.out.println("Please enter Information of ticket you want to reserve");
+        System.out.println("Enter idTicket:");
+        int id = input.nextInt();
+        String[] ticketInformation = ticketService.getTicketInformation(id);
+        if (ticketInformation == null){
+            System.out.println("You enter a wrong id Ticket!");
+            return;
+        }
+        filmName = ticketInformation[0];
+        System.out.print("How many ticket you want to buy?:");
+        numberTickets = input.nextInt();
+        if(numberTickets > Integer.parseInt(ticketInformation[1])){
+            System.out.println("you enter a number bigger than numberOfTicket we have");
+            return;
+        }
+        if(ticketService.updateNumberOfTicket(id,Integer.parseInt(ticketInformation[1]) - numberTickets) == 0 ){
+            System.out.println("Something Wrong");
+            return;
+        }
+        priceAll = calcPriceAll(numberTickets,Integer.parseInt(ticketInformation[2]));
+        int allBuy = ticketService.allBuyTicket(id) + numberTickets;
+        ticketService.updateNumberTicketBuy(id,allBuy);
+        Basket basket = new Basket(username,id,filmName,numberTickets,priceAll);
+        if(basketService.importTicket(basket) != 0)
+            System.out.println("Ticket you Enter successful add to you Basket!");
+        else
+            System.out.println("Something Wrong!");
+    }
+
+    public int calcPriceAll(int number,int price){
+        input.nextLine();
+        return number * price;
+        /*
+        System.out.print("if you have a OFF code,Please enter it:");
+        String off = input.nextLine();
+        if(manager.searchOffCode(off) == 0 ){
+            System.out.println("You dont have a OFF code,and you ticket calc with normal price!");
+            return number*price;
+        }
+        else{
+            String[] array =off.split("-");
+            int mountPercentage = Integer.parseInt(array[1]);
+            double finalPercentage = (100.0 - mountPercentage ) / 100;
+            return (int) ((number*price) * finalPercentage);
+        }
+
+         */
+
+    }
 
 
 
